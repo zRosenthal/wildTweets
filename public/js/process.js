ef0d3e75a3be2012411a29bd3d456347adbb6782
Zachary Rosenthal <rosent76@msu.edu>
2015-11-22 03:38:51 -0600

    here voices in my head think im skitzifrenickcakdj

--- public/js/process.js
+++ public/js/process.js
@@ -3,8 +3,21 @@ function process(count) {
 
     var dataH = $('#display_header');
 
+    dates = getCurrentDate();
+
+    for(var i = 0; i<7; i++) {
+
+        makeRequest(dates[i],dates[i+1]);
+
+    }
+
+}
+
+
+function makeRequest(date1, date2) {
+
     $.ajax({
-        url: '/process/'+ query +'/'+ count,
+        url: '/process/'+ query +'/'+ date1 + '/' + date2,
         method: 'get',
         success: function (data) {
             console.log(data);
@@ -22,7 +35,32 @@ function process(count) {
 
         }
     })
+
 }
+
+function getCurrentDate() {
+
+    var dates = [];
+
+    var date = Date();
+
+    for (var i = 0; i<=7; i++) {
+
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + (d.getDate() -7 + i),
+            year = d.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+        dates.push([year, month, day].join('-'));
+
+    }
+
+    return dates;
+
+}
+
 function queueFlyingMonkey() {
 
     var monkey = $("#monkey");
