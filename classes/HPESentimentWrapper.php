27d4e714a2c53dbee3005285bd3add030cabd47b
Ryan Cornillie <rccornillie@gmail.com>
2015-11-22 00:32:26 -0500

    weiner2

--- classes/HPESentimentWrapper.php
+++ classes/HPESentimentWrapper.php
@@ -60,7 +60,7 @@ class HPESentimentWrapper {
             }
             if($sentimentVal == 0)
             {
-                echo $twat . "<br><br>";
+                echo "hack" . $twat . "<br><br>";
                 $size = $size-1;
             }
             $sum = $sum + $sentimentVal;
