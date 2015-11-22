de0808c9d2122c5db09460818afe398ddb263350
Ryan Cornillie <rccornillie@gmail.com>
2015-11-21 21:02:58 -0500

    Added sentiment value json decoder

---
+++ public/HPEEntityWrapper.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rosent76
+ * Date: 11/21/15
+ * Time: 8:17 PM
+ */
+
+class HPEEntityWrapper {
+
+} 
--- public/index.php
+++ public/index.php
@@ -47,7 +47,9 @@ $app->get('/callback', function () use ($app) {
 $app->get('/app', function () use ($app) {
 
     $HPE = new HPESentimentWrapper();
-    echo $HPE->GetSentimentJson("I really fucking hate my mother");
+    $vala = $HPE->GetSentimentValue("I really fucking hate my mother");
+    $valb = $HPE->GetSentimentValue("I really love my mother");
+    echo $vala. " " . $valb . "<br>".($vala+$valb)/2;
 //    $twitter = new TwitterRequest();
 //    $app->log->info(json_encode($twitter));
 //    $return = $twitter->requestTweet('search/tweets', array('q' => 'superbowl', 'result_type' => 'recent'));
