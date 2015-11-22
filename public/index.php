e82a66b7b6473b5b99374040a5299ac0be1a0c3b
Zachary Rosenthal <rosent76@msu.edu>
2015-11-21 20:47:25 -0600

    trying to get shit to work

--- public/index.php
+++ public/index.php
@@ -41,7 +41,23 @@ $app->get('/', function () use ($app) {
 
 $app->get('/twitterTest', function () use ($app) {
 
-    echo "works bitch";
+    $correctText = str_replace(" ","+",$text);
+    $query = 'https://google.com';
+    // create curl resource
+    $ch = curl_init();
+
+    // set url
+    curl_setopt($ch, CURLOPT_URL, $query);
+
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // $output contains the output string
+    $output = curl_exec($ch);
+
+    // close curl resource to free up system resources
+    curl_close($ch);
+    echo json_encode($output);
 
 });
 
